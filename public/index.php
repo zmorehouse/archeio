08a3e20bdb88dd7167bb593eb635b9f5c9e391c2
zacmorehouselogin@gmail.com <zacmorehouselogin@gmail.com>
2026-01-15 15:37:17 +1100

    Memory fixes

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
+ini_set('memory_limit', '384M');
+
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
